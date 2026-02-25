c9e3c4a818f88dd42233332f653c449972d3aca3
Tehice02 <155299117+Tehice02@users.noreply.github.com>
2026-02-25 14:32:54 +0800

    delete

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-    BASE_DIR, "database", "nids.db"
-)
-
